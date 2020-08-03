aa38dadecf7f5c354a97d31b80141441634479a7
Kyle.Tryon <Kyle.Tryon@outlook.com>
2020-08-02 23:56:32 -0400

    add build workflow for gh-pages

---
+++ build/_dist_/objects/consumables/fishFood.js
@@ -0,0 +1,11 @@
+import "/web_modules/phaser.js";
+export default class FishFood extends Phaser.GameObjects.Sprite {
+  constructor(params) {
+    super(params.scene, params.x, params.y, "fish_food", params.frame);
+    this.foodValue = 20;
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this);
+    this._body = this.body;
+    this._body.collideWorldBounds = true;
+  }
+}
