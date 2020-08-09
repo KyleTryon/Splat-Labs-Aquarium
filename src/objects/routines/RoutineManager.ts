56244c00b20e160cab74e57bc60e7b567868769c
Kyle.Tryon <Kyle.Tryon@outlook.com>
2020-08-09 01:06:02 -0400

    cleanup routine manager

--- src/objects/routines/RoutineManager.ts
+++ src/objects/routines/RoutineManager.ts
@@ -13,45 +13,32 @@ interface params {
  * @class
  */
 export default class RoutineManager {
-  routines: Array<IRoutines>
+  routines: Array<IRoutines> = []
   fish: Fish
 
-  // //Routines
-  // wander: Wander
-  // huntFood: HuntFood
 
   constructor(params: params) {
-    let _routines = []
     this.fish = params.fish
-    // this.wander = new Wander({
-    //   creature: this.creature
-    // })
-    _routines.push(new Wander({
+    this.routines.push(new Wander({
       fish: this.fish
     }))
-    _routines.push(new HuntFood({
+    this.routines.push(new HuntFood({
       fish: this.fish
     }))
-    _routines.push(new DropLoot({
+    this.routines.push(new DropLoot({
       fish: this.fish
     }))
-    this.routines = _routines
   }
 
   execute(): void {
     // Determine the routine to run and call execute() on it
     let highestPriority = this.routines.sort((routineA: IRoutines, routineB: IRoutines) => {
-      let priorityA = routineA.getPriority()
-      let priorityB = routineB.getPriority()
-      if (priorityA < priorityB) {
+      if (routineA.getPriority() <= routineB.getPriority()) {
         return 1
-      } else if (priorityA == priorityB) {
-        return 0
       } else {
         return -1
       }
     })
-
     highestPriority[0].execute()
   }
 }
