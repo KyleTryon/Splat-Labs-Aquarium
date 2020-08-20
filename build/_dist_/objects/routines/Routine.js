e0083a135858b47d858c5dea3f63bfca8139e05f
Kyle.Tryon <Kyle.Tryon@outlook.com>
2020-08-20 01:13:16 -0400

    build for deployment.

---
+++ build/_dist_/objects/routines/Routine.js
@@ -0,0 +1,1 @@
+export default class t{constructor(i){this.priority=i.priority||.1,this.name=i.name,this.fish=i.fish}calcPriority(){}execute(){}}
--- build/_dist_/objects/routines/fish/Wander.js
+++ build/_dist_/objects/routines/fish/Wander.js
@@ -1,17 +1,1 @@
-export default class Wander {
-  constructor(params) {
-    this.name = "Wander";
-    this.fish = params.fish;
-    this.priority = 0.1;
-  }
-  calcPriority() {
-    this.priority = 0.1;
-  }
-  execute() {
-    if (this.fish.getDistanceToTartget() < 4) {
-      this.fish.target = this.fish.scene.getRandomPoint();
-    } else {
-      this.fish.swimToTarget();
-    }
-  }
-}
+export default class i{constructor(t){this.name="Wander",this.fish=t.fish,this.priority=.1}calcPriority(){this.priority=.1}execute(){this.fish.getDistanceToTartget()<4?this.fish.target=this.fish.scene.getRandomPoint():this.fish.swimToTarget()}}
